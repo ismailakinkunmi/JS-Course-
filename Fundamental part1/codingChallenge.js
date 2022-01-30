604d576e082882f2101590f348411048f857e60e
Adekunle Ismail <adismail4@gmail.com>
2022-01-30 21:43:32 +0100

    Modify codingChallenge.js

--- Fundamental part1/codingChallenge.js
+++ Fundamental part1/codingChallenge.js
@@ -14,3 +14,43 @@ if (bodyMassIndexIsmail > bodyMassIndexAdekunle) {
 } else {
   console.log(`Adekunle BMI ${bodyMassIndexAdekunle} is higher than ismail`);
 }
+
+console.log("_________________________");
+console.log("coeasion");
+console.log("_________________________");
+const value1 = "5";
+const value2 = 9;
+let sum = Number(value1) + value2;
+
+console.log(sum);
+
+const money = 12;
+
+if (
+  money === 0 ||
+  money === null ||
+  money === NaN ||
+  money == "" ||
+  money == undefined
+) {
+  console.log(
+    `this value ${money} is a falsy value please provide a truthy value `
+  );
+} else {
+  console.log(`This is a truthy value ${money}`);
+}
+
+const dolphinScore = 96 + 108 + 89;
+const kalasScore = 91 + 110 + 88;
+
+const dolphinAverageScore = dolphinScore / 3;
+const kalasAverageScore = kalasScore / 3;
+// 91 + 110 + 88;
+
+if (dolphinAverageScore > kalasAverageScore) {
+  console.log(`Dolphin average score ${dolphinAverageScore} is a winner`);
+} else if (kalasAverageScore > dolphinAverageScore) {
+  console.log(`kalas average score ${kalasAverageScore} is a winner`);
+} else {
+  console.log("both play draw no winner");
+}
