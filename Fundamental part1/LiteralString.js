55733a59855f2b69f6450b0532f2c40c2f8503fd
Adekunle Ismail <adismail4@gmail.com>
2022-01-30 13:05:45 +0100

    First commit

---
+++ Fundamental part1/LiteralString.js
@@ -0,0 +1,24 @@
+const firstName = "Adekunle";
+const lastname = "Ismail";
+const job = "Software web developer";
+const year = 2050;
+const birthDate = 1986;
+
+console.log(
+  `My name is ${firstName} ${lastname} a ${job} of ${
+    year - birthDate
+  } years old!`
+);
+
+console.log(
+  `Some developer uses backtick in place of string but I don't use it.`
+);
+
+console.log(`Using string
+to write
+multiline
+with backtick`);
+
+console.log(
+  "Using string to write\n multiline\n with interpolation double string"
+);
